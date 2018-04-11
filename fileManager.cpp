a78751dabd792d6361d25cae4b6f1f90c7cd0a11
Jared <jaredping@yahoo.com>
2018-04-11 18:19:44 +0200

    Initial file manager class

--- fileManager.cpp
+++ fileManager.cpp
@@ -1,7 +1,11 @@
 #include "fileManager.h"
 
-fileManager::fileManager(string fileName, string delimeter=",")
+fileManager::fileManager(string fileName, string key, string delimeter=",")
 {
+    _file_name = fileName;
+    if (delimeter != ","){
+        _delimeter = delimeter;
+    }
 }
 
 fileManager::~fileManager()
@@ -9,7 +13,16 @@ fileManager::~fileManager()
 
 }
 
+string fileManager::readFile()
+{
+	ifstream file(_file_name); // pass file name as argment
+	string linebuffer;
 
+	while (file && getline(file, linebuffer)){
+		if (linebuffer.length() == 0)continue;
+		
+	}
+}
 
 vector<string> fileManager::split(char *phrase, string delimiter)
 {
--- fileManager.h
+++ fileManager.h
@@ -1,13 +1,28 @@
 #ifndef FILEMANAGER_H
 #define FILEMANAGER_H
 
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
 class fileManager
 {
 public:
-	fileManager(string fileName, string delimeter);
+	fileManager(string fileName, string key, string delimeter);
 	~fileManager();
 
+	string readFile();
 	vector<string> split(char *phrase, string delimiter);
+
+private:
+	string _file_name;
+	string _delimeter;
+	string _key;
+	vector<string> _data;
 };
 
 #endif // FILEMANAGER_H

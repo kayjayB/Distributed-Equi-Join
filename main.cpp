b17fc6ca96ade21952d2cad33e237358e688931b
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-04-11 20:22:55 +0200

    Loop to check all keys

--- main.cpp
+++ main.cpp
@@ -20,8 +20,6 @@ int main()
 	test.hasher->AddItem("KJ", "23");
 	test.hasher->AddItem("Lara", "Potot");
 	test.hasher->AddItem("KJ", "230");
-	test.hasher->AddItem("Lara", "230");
-	test.hasher->AddItem("Jared", "293");
 	test.hasher->AddItem("Jared", "2254");
 	test.hasher->AddItem("Matt", "12345");
 	test.hasher->AddItem("Matt", "98765");
@@ -35,7 +33,26 @@ int main()
 	string test1 = "Lara,23,968";
 	
 	string key = "KJ";
+	vector<string> linesOfFile1;
+	vector<string> keys;
 	
-	test.query(key, test1);
+	linesOfFile1.push_back("NewName, lemons, 90");
+	linesOfFile1.push_back("KJ, coffee, 68");
+	linesOfFile1.push_back("Lara, potatoes, 21");
+	linesOfFile1.push_back("Jared, nandos, 67");
+	linesOfFile1.push_back("Matt, sugar, 00");
 	
+	keys.push_back("NewName");
+	keys.push_back("KJ");
+	keys.push_back("Lara");
+	keys.push_back("Jared");
+	keys.push_back("Matt");
+	
+	for (int i=0; i<linesOfFile1.size();i++)
+	{
+		test.query(keys[i], linesOfFile1[i]);
+	}
+	
+
+//	test.query(key, test1);
 }

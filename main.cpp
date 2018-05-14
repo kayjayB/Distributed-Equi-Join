0a0bf93297bca4c85315b7e090ec03ad6bac137a
jared <jaredp@hyperiondev.com>
2018-05-14 02:53:14 +0200

    Improved implementation

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include <sys/time.h>
 
 #include "hashFunction.h"
-#include "joinManager.h"
 #include "fileManager.h"
 
 #include "mpi.h"
@@ -17,8 +16,36 @@ using namespace std;
 using std::shared_ptr;
 using std::make_shared;
 
+vector<string> join(string key, string lineFromR1, vector<string> linesFromR2)
+{	
+	string result;
+	vector<string> results;
+	
+	for (auto i:linesFromR2)
+	{
+		size_t startOfKey = i.find(key);
+		size_t endOfKey = startOfKey + key.length();
+		result = lineFromR1 + i.substr(0,startOfKey) + i.substr(endOfKey+1, (i.length()-endOfKey));
+		results.push_back(result);
+	}
+	return results;
+}
+
+vector<string> query(shared_ptr<hashFunction> hasher, string key, string lineFromR1)
+{
+	vector<string> queryResults = hasher->FindValue(key); 
+	vector<string> results;
+	
+	if (!queryResults.empty())
+	{
+		results = join(key,lineFromR1 ,queryResults);
+	}
+	return results;
+}
+
 int main(int argc, char *argv[])
 {
+	//Bomp_set_num_threads(8);
 	int numprocs, rank, namelen;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 	int iam = 0, np = 1;
@@ -31,108 +58,75 @@ int main(int argc, char *argv[])
 	struct timeval tv1, tv2;
     gettimeofday(&tv1, NULL);
 
+	int prev_length = 0;
+	int length_track = 0;
+
 	if (rank == 0) {
 		fileManager fileHandler;
 		fileHandler.deleteFile("joinedFile.txt");
 
-		vector<string> linesOfFile1, linesOfFile2, results;
-
-		linesOfFile1 = fileHandler.readFile("smallInput1.txt",1);
-		linesOfFile2 = fileHandler.readFile("smallInput2.txt",1);
-
-		int length_file_1 = linesOfFile1.size();
-		int length_file_2 = linesOfFile2.size();
-		int prev_length = 0;
-		int length_track = 0;
-
-		#pragma omp critical
-		for (auto i = 1; i < numprocs; i++){
-			int process_length_file_2 = (length_file_2 / (numprocs - 1.0));
-			if (process_length_file_2 % 2 != 0) process_length_file_2 += 1;
-			length_track += process_length_file_2;
-
-			if (i == numprocs-1){
-				if (length_track < length_file_2){
-					process_length_file_2 = process_length_file_2 + length_file_2 - length_track;
-					length_track += length_file_2 - length_track;
-				}
-				else if (length_track > length_file_2){
-					process_length_file_2 = process_length_file_2 - (length_track - length_file_2);
-					length_track = length_file_2;
-				}
-			}
-
-			MPI_Send(&length_file_1, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-			MPI_Send(&process_length_file_2, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-
-			for (auto x=0; x < length_file_1; x++){
-				const char* line_ptr = linesOfFile1[x].c_str();
-				string line = linesOfFile1[x].c_str();
-				int line_length = line.length();
-				MPI_Send(&line_length, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-				MPI_Send(line_ptr, line_length, MPI_CHAR, i, 0, MPI_COMM_WORLD);
-			}
-
-			for (auto y = prev_length; y < length_track; y++){
-				const char* line_ptr = linesOfFile2[y].c_str();
-				string line = linesOfFile2[y].c_str();
-				int line_length = line.length();
-				MPI_Send(&line_length, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-				MPI_Send(line_ptr, line_length, MPI_CHAR, i, 0, MPI_COMM_WORLD);
-			}
-			prev_length = length_track;
-
-			int results_length;
-
-			MPI_Recv(&results_length, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-
-			for (auto i=0; i < results_length; i++){
-				int line_length;
-				char char_array[1024];
-				memset(char_array, 0, sizeof(char_array));
-				MPI_Recv(&line_length, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-				MPI_Recv(&char_array[0], line_length, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-				string line(char_array);
-				results.push_back(line);
-			}
-		}
-
 		ofstream myFile;
 		myFile.open("joinedFile.txt", std::ios::app);
 
-		for (auto i=results.begin(); i != results.end(); i++){
-			myFile << *i << "\n";
+		#pragma omp critical
+		for (auto i = 1; i < numprocs; i++){
+			int results_length;
+
+			MPI_Recv(&results_length, 1, MPI_INT, i, i, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+			for (auto j=0; j < results_length; j++){
+				int line_length;
+				char char_array[1024];
+				memset(char_array, 0, sizeof(char_array));
+				MPI_Recv(&line_length, 1, MPI_INT, i, i, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+				MPI_Recv(&char_array[0], line_length, MPI_CHAR, i, i, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+				string line(char_array);
+				myFile << line << endl;
+			}
 		}
+
 		myFile.close();
 	}
 	else {
-		int length_file_1, length_file_2;
-		vector<string> linesOfFile1, linesOfFile2, results;
-		vector<vector<string>> buffer;
-		joinManager test;
+		shared_ptr<hashFunction> hasher = make_shared<hashFunction>();
+		int length_file_1, length_file_2, process_length_file_2, line_count, length_file_results;
+		vector<string> linesOfFile1, linesOfFile2, linesOfFile2_node, results;
+		vector<vector<string>> vec_results_final;
+		fileManager fileHandler;
 
-		MPI_Recv(&length_file_1, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		MPI_Recv(&length_file_2, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		linesOfFile1 = fileHandler.readFile("100thou1.txt",1);
+		length_file_1 = linesOfFile1.size();
+		linesOfFile2 = fileHandler.readFile("100thou2.txt",1);
+		length_file_2 = linesOfFile2.size();
 
-		for (auto i=0; i < length_file_1; i++){
-			int line_length;
-			char char_array[1024];
-			memset(char_array, 0, sizeof(char_array));
-			MPI_Recv(&line_length, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			MPI_Recv(&char_array[0], line_length, MPI_CHAR, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			string line(char_array);
-			linesOfFile1.push_back(line);
+		process_length_file_2 = (length_file_2 / (numprocs - 1.0));
+		
+		if (process_length_file_2 % 2 != 0) process_length_file_2 += 1;
+		length_track = rank * process_length_file_2;
+		prev_length = (rank-1) * process_length_file_2;
+
+		if (rank == numprocs-1){
+			if (length_track > length_file_2){
+				process_length_file_2 = process_length_file_2 - (length_track - length_file_2);
+				length_track = length_file_2;
+			}
+			else if (length_track < length_file_2){
+				process_length_file_2 = process_length_file_2 + length_file_2 - length_track;
+				length_track += length_file_2 - length_track;
+			}
 		}
 
-		for (auto i=0; i < length_file_2; i++){
-			int line_length;
-			char char_array[1024];
-			memset(char_array, 0, sizeof(char_array));
-			MPI_Recv(&line_length, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			MPI_Recv(&char_array[0], line_length, MPI_CHAR, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			string line(char_array);
-			linesOfFile2.push_back(line);
+		line_count = 0;
+
+		for (auto line : linesOfFile2){
+			if (line_count >= length_track) break;
+			if (line_count >= prev_length && line_count < length_track){
+				linesOfFile2_node.push_back(line);
+			}
+			line_count++;
 		}
+
+		length_file_2 = linesOfFile2_node.size();
 
 		#pragma omp parallel default(shared) private(iam, np) 
 		{
@@ -141,35 +135,41 @@ int main(int argc, char *argv[])
 			printf("Working on thread %d out of %d from process %d out of %d on %s\n", iam, np, rank, numprocs, processor_name);
 
 			#pragma omp for
-			for (unsigned int i=0; i< linesOfFile1.size(); i = i+2)
+			for (auto i = 0; i < length_file_1; i = i + 2)
 			{
-				//int tid = omp_get_thread_num();  
-				//printf("Hello World from thread = %d\n", tid);  
 				string key = linesOfFile1[i];
 				string value = linesOfFile1[i+1];
-				test.hasher -> AddItem(key,value);
+				hasher -> AddItem(key,value);
+			}
+
+			vector<vector<string>> vec_results;
+			
+			#pragma omp for nowait schedule(static)
+			for (auto i = 0; i < length_file_2; i = i + 2) { 
+				vec_results.push_back(query(hasher, linesOfFile2_node[i], linesOfFile2_node[i+1]));
 			}
 			
-			#pragma omp single
-			for (unsigned int i=0; i<linesOfFile2.size();i = i+2)
-			{
-			//	int tid = omp_get_thread_num();  
-			//	printf("Hello World from thread = %d\n", tid); 
-				test.query(linesOfFile2[i], linesOfFile2[i+1]);
+			#pragma omp for schedule(static) ordered
+			for(int i=0; i<omp_get_num_threads(); i++) {
+				#pragma omp ordered
+				vec_results_final.insert(vec_results_final.end(), vec_results.begin(), vec_results.end());
 			}
 		}
 
-		results = test.getResults();
+		for(auto && v : vec_results_final){
+			results.insert(results.end(), v.begin(), v.end());
+		}
 
-		int length_file_results = results.size();
-		MPI_Send(&length_file_results, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		length_file_results = results.size();
+
+		MPI_Send(&length_file_results, 1, MPI_INT, 0,rank, MPI_COMM_WORLD);
 
 		for (auto x=0; x < length_file_results; x++){
 			const char* line_ptr = results[x].c_str();
 			string line = results[x].c_str();
 			int line_length = line.length();
-			MPI_Send(&line_length, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-			MPI_Send(line_ptr, line_length, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+			MPI_Send(&line_length, 1, MPI_INT, 0, rank, MPI_COMM_WORLD);
+			MPI_Send(line_ptr, line_length, MPI_CHAR, 0, rank, MPI_COMM_WORLD);
 		}
 	}
 
@@ -177,8 +177,6 @@ int main(int argc, char *argv[])
 
     printf("Time to join: ");
     printf ("%f seconds\n", (double) (tv2.tv_usec - tv1.tv_usec) / CLOCKS_PER_SEC + (double) (tv2.tv_sec - tv1.tv_sec));
-
-    printf("\n");
 
 	MPI_Finalize();
 }

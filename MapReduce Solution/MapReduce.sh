2e869e62d1e271f05cae0e0ce2348c1444058521
jared <jaredping@yahoo.com>
2018-05-15 15:11:52 +0200

    MapReduce script

--- MapReduce Solution/MapReduce.sh
+++ MapReduce Solution/MapReduce.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-FILE1 = $1
-FILE2 = $2
-COLUMN = $3
-cp $FILE1 input1.txt
+#!/bin/sh
 
-cp $FILE2 input2.txt
+cp $1 input1.txt
+
+cp $2 input2.txt
 
 cp input1.txt input11.txt
 
@@ -12,7 +10,7 @@ cp input2.txt input22.txt
 
 g++-5 -g -O2 -Wall -D_LINUX_ equi_join.cpp -fstrict-aliasing -Wstrict-aliasing -lpthread -lrt -fpermissive -L./ -lphoenix -I./ -o equi_join
 
-MR_NUMTHREADS=4 ./equi_join input1.txt input2.txt input11.txt input22.txt $COLUMN
+MR_NUMTHREADS=4 ./equi_join input1.txt input2.txt input11.txt input22.txt $3
 
 rm input1.txt
 rm input11.txt

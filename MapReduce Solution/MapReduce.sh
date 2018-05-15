ea19ea71a2681b4ae9ba84ebcaa08ca8a282383b
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-05-15 14:14:51 +0200

    Put everything in folders

--- MapReduce Solution/MapReduce.sh
+++ MapReduce Solution/MapReduce.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+FILE1 = $1
+FILE2 = $2
+COLUMN = $3
+cp $FILE1 input1.txt
 
-cp 1milLines1.txt input1.txt
-
-cp 1milLines2.txt input2.txt
+cp $FILE2 input2.txt
 
 cp input1.txt input11.txt
 
@@ -10,7 +12,7 @@ cp input2.txt input22.txt
 
 g++-5 -g -O2 -Wall -D_LINUX_ equi_join.cpp -fstrict-aliasing -Wstrict-aliasing -lpthread -lrt -fpermissive -L./ -lphoenix -I./ -o equi_join
 
-MR_NUMTHREADS=4 ./equi_join input1.txt input2.txt input11.txt input22.txt 1
+MR_NUMTHREADS=4 ./equi_join input1.txt input2.txt input11.txt input22.txt $COLUMN
 
 rm input1.txt
 rm input11.txt
